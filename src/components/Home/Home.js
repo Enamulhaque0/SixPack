fe53534d263e13648e6b66704d7255dd523e2ff9
Enamulhaque0e <enamulhaq243@gmail.com>
2022-09-29 11:03:40 +0600

    break time added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,50 @@
 import { useState } from "react";
 import Items from "../Items/Items";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const Home = () => {
- const [exercise,setExercise] =useState([])
- 
-const sum = exercise.reduce((accumulator, object) => {
-  return accumulator + object.time;
-}, 0);
+  const [exercise, setExercise] = useState([]);
+  const [breakTime, setBreak] = useState(0);
 
-const tost=()=>{
-  Swal.fire(
-    'Good job!',
-    'Activity Completed!',
-    'success'
-  )
-}
+  const handleTen = (e) => {
+    setBreak(e.target.value);
+  };
 
-// console.log(exercise);
+  const handleTwenty = (e) => {
+    setBreak(e.target.value);
+  };
+
+  const handleThirty = (e) => {
+    setBreak(e.target.value);
+  };
+
+  const handleForty = (e) => {
+    setBreak(e.target.value);
+  };
+
+  const sum = exercise.reduce((accumulator, object) => {
+    return accumulator + object.time;
+  }, 0);
+
+  const tost = () => {
+    Swal.fire("Good job!", "Activity Completed!", "success");
+  };
+
+  // console.log(exercise);
   return (
     <div className="mt-6">
-      <div className="flex  ms-12">
+      <div className="flex  ms-8">
         <div className=" text-center">
           <h1 className="text-3xl">Select today’s exercise</h1>
 
-          <Items exercise={exercise} setExercise={setExercise} ></Items>
+          <Items exercise={exercise} setExercise={setExercise}></Items>
         </div>
 
         <div className=" w-30 text-center">
           <div>
             <div>
               <div className="card w-full h-full bg-base-100 shadow-xl">
-                <div className="avatar m-8 flex justify-around">
+                <div className="avatar  flex justify-around">
                   <div className="w-24 rounded ">
                     <img src="https://placeimg.com/192/192/people" />
                   </div>
@@ -48,20 +61,46 @@ const tost=()=>{
                   </div>
                   <h1 className="text-1xl">Add A Break</h1>
                   <div className="flex justify-around gap-3 ">
-                    <button className="btn btn-xs">10s</button>
-                    <button className="btn btn-xs">20s</button>
-                    <button className="btn btn-xs">30s</button>
-                    <button className="btn btn-xs">40s</button>
+                    <button
+                      onClick={handleTen}
+                      value="10"
+                      className="btn btn-xs"
+                    >
+                      10s
+                    </button>
+                    <button
+                      onClick={handleTwenty}
+                      value="20"
+                      className="btn btn-xs"
+                    >
+                      20s
+                    </button>
+                    <button
+                      onClick={handleThirty}
+                      value="30"
+                      className="btn btn-xs"
+                    >
+                      30s
+                    </button>
+                    <button
+                      onClick={handleForty}
+                      value="40"
+                      className="btn btn-xs"
+                    >
+                      40s
+                    </button>
                   </div>
                   <h1 className="mt-8 text-1xl">Exercise Details</h1>
                   <h1 className="mt-7 bg-slate-600 p-3 text-white">
                     Exercise time: {sum} seconds
                   </h1>
                   <h1 className="mt-7 bg-slate-600 p-3 text-white">
-                    Break time : 15 seconds
+                    Break time : {breakTime} seconds
                   </h1>
                   <div className="card-actions">
-                    <button onClick={tost} className="btn btn-wide mt-7" >Activity Completed</button>
+                    <button onClick={tost} className="btn btn-wide mt-7">
+                      Activity Completed
+                    </button>
                   </div>
                 </div>
               </div>
